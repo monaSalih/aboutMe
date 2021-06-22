b95d89618cc761da7fa6459816f5c3b1bfafdb79
monaSalih <monaalobisat@gmail.com>
2021-06-22 13:38:05 +0300

    work

--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
 'use strict';
-// let x=prompt("enter your name please");
+ let x=prompt("enter your name please");
 
-// alert ("welcome "+" "+x+" "+"in our webpage we hope to enjoy in this trip");
+ alert ("welcome "+" "+x+" "+"in our webpage we hope to enjoy in this trip");
 
-// alert('please read following message and after that you will answer one qustion')
+ alert('please read following message and after that you will answer one qustion')
 
-// alert('ilove small cars like (Ford ,Toyota and tesla)'+"  "+",love health food ,"+"  "+",and my height is 153."+"  "+",inlove with node color"+"  "
-// +" ,reading book"+"  "+" intresting on technical.");
+ alert('ilove small cars like (Ford ,Toyota and tesla)'+"  "+",love health food ,"+"  "+",and my height is 153."+"  "+",inlove with node color"+"  "
+ +" ,reading book"+"  "+" intresting on technical.");
 
-//     let c=confirm("did i love fast food")
+     let c=confirm("did i love fast food")
 
-//     if (c== false){
-//         console.log('realy happy for your answer'); 
-//     }
-//     else{
-//         console.log('No i didnt like fast food not good for health'); 
-//     }
+    if (c== false){
+        console.log('realy happy for your answer'); 
+     }
+    else{
+         console.log('No i didnt like fast food not good for health'); 
+     }
     
-// let a1=prompt("did i prefare big car answer(yse/no)".toLowerCase());
-// switch(a1){
-//     case 'yes':
-//          console.log('yes i love small car'); 
-//         break;
-//         case 'no':
-//             console.log('no didnt like big car'); 
-//               break;
-//           default:
-//             console.log('hmmm try another answer'); 
-//             break;
+let a1=prompt("did i prefare big car answer(yse/no)".toLowerCase());
+ switch(a1){
+     case 'yes':
+         console.log('yes i love small car'); 
+         break;
+         case 'no':
+             console.log('no didnt like big car'); 
+               break;
+           default:
+             console.log('hmmm try another answer'); 
+             break;
 
-//     }
+     }
 
-//     let a2=prompt("did i love read big book?(YSE/NO)".toLowerCase());
-// switch(a2){
-//     case 'yes':
-//          console.log('i hope you love reading olso'); 
-//         break;
-//         case 'no':
-//             console.log('not prefer it') ;
-//               break;
-//           default:
-//             console.log('hmmm try another answer'); 
-//             break;
+     let a2=prompt("did i love read big book?(YSE/NO)".toLowerCase());
+ switch(a2){
+     case 'yes':
+          console.log('i hope you love reading olso'); 
+         break;
+         case 'no':
+             console.log('not prefer it') ;
+               break;
+           default:
+             console.log('hmmm try another answer'); 
+             break;
 
 //     }
 
